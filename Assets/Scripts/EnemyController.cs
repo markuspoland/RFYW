3168b147259cc0aa38d68e290b41c5243f0c60d8
KULA <MAREK.KULA@ing.com>
2022-01-23 17:48:45 +0100

    more shooting, blood etc

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,8 +11,22 @@ public class EnemyController : MonoBehaviour
     public EnemySide enemySide;
     public EnemySpawner enemySpawner;
 
+    public Material normalMaterial;
+    public Material hitMaterial;
+
+    public AudioClip enemyShoot;
+
+    public GameObject[] bloodEffects;
+    public GameObject chunkEffect;
+    public Transform body;
+
+    PlayerController player;
+    AudioSource audioSource;
     private void OnEnable()
     {
+        GetComponentInChildren<Renderer>().material.color = normalMaterial.color;
+        audioSource = GetComponent<AudioSource>();
+        player = FindObjectOfType<PlayerController>();
         transform.position = enemySpawner.transform.position;
         timer = 1f;
         health = 100;
@@ -49,13 +63,37 @@ public class EnemyController : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+        StartCoroutine(GetHit());
+        Vector3 mousePos = Input.mousePosition;
+        Instantiate(bloodEffects[Random.Range(0, bloodEffects.Length - 1)], GetPosition(), Quaternion.identity);
 
         if (health <= 0)
         {
             enemySpawner.isSpawned = false;
             gameObject.SetActive(false);
+            Instantiate(chunkEffect, GetPosition(), Quaternion.identity);
             
         }
+    }
+
+    public void ShootPlayer()
+    {
+        audioSource.PlayOneShot(enemyShoot);
+        player.TakeDamage(Random.Range(5, 12));
+        
+        
+    }
+
+    IEnumerator GetHit()
+    {
+        GetComponentInChildren<Renderer>().material.color = hitMaterial.color;
+        yield return new WaitForSeconds(0.1f);
+        GetComponentInChildren<Renderer>().material.color = normalMaterial.color;
+    }
+
+    Vector3 GetPosition()
+    {
+        return Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, body.position.z + 22));
     }
 
 }
